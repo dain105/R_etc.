776917bb92f1a73fbc0b7f51a7fc36837acca51b
Dain <44390404+dain105@users.noreply.github.com>
2021-02-19 10:34:25 +0900

    Interpolation with IDW(inverse distance weighted) and drawing raster plot in r
    Interpolation with IDW(inverse distance weighted) and drawing raster plot in r

---
+++ idwmap.R
@@ -0,0 +1,20 @@
+library(sp)
+library(gstat)
+library(raster)
+library(maps)
+library(mapdata)
+library(ggmap)
+
+
+gg2<-data
+coordinates(gg2)<-c("lon","lat")
+grd<-as.data.frame(spsample(gg2, "regular",n=50000))
+names(grd)<-c("lon","lat")
+coordinates(grd)<-c("lon","lat")
+gridded(grd)=TRUE
+fullgrid(grd)<-TRUE
+
+mse.idw <-gstat::idw(mean.mtx~1, gg2, newdata=grd, idp=2)
+r<- raster(mse.idw)
+plot(r)
+
